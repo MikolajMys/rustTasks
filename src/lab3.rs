c9a77f1c13e2f58590cdbd8ed814b2ac1fa6196a
miki <mikolaj.my@gmail.com>
2024-04-11 11:17:51 +0200

    error

--- src/lab3.rs
+++ src/lab3.rs
@@ -30,13 +30,13 @@ pub fn swap_sort_three_args(x: i32, y: i32, z: i32){
 //Zadanie 3 Stwórz generator liczb pseudolosowych, którego ziarno przechowywane będzie na zewnątrz i podawane w pierwszym parametrze, a w parametrze drugim i trzecim podawany będzie przedział losowanych liczb.
 // fn rand(seed: &mut ..., min_rand: ..., max_rand: ...) -> ...:
 pub fn rand(seed: &mut i128, min_rand: i128, max_rand: i128) -> i128{
-    *seed = (60 * *seed + 50) % 4355;
+    *seed = (75 * *seed + 70) % 6553;
     //println!("{}", *seed);
     *seed % (max_rand-min_rand+1) + min_rand
 }
 //Zadanie 4 Napisz funkcję swap_arr(arr: ..., i: usize, j: usize),
 // która zamieni wartości dwóch podanych elementów pewnej tablicy:
-pub fn swap_arr(arr: &mut [i32;5], i: usize, j: usize){
+pub fn swap_arr(arr: &mut [i32], i: usize, j: usize){
     let temp: i32 = arr[i-1];
     arr[i-1] = arr[j-1];
     arr[j-1] = temp;
@@ -44,6 +44,13 @@ pub fn swap_arr(arr: &mut [i32;5], i: usize, j: usize){
 
 //Zadanie 5 Stwórz funkcję rand_perm(arr: ..., seed: ...),
 // permutującą w miejscu w sposób losowy wartości tablicy przekazanej w argumencie:
+pub fn rand_perm(arr: &mut [i32], seed: &mut i128){
+    for i in 0..arr.len() {
+        let j = rand(seed, 0, (arr.len() - 1) as i128) as usize;
+        swap_arr(arr, i, j);
+        println!("{j}");
+    }
+}
 
 //Inna wersja zadań:
 //Zad 1 Napisz funkcję d2((x, y), (x, y)) -> f32, która obliczy dystans pomiędzy punktami w przestrzeni R^2.
--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,17 @@ fn sort(a:&mut u32, b:&mut u32, c:&mut u32){
 //     let temp:[usize;len];
 //
 // }
+// fn wizytowka(imie: &String, nazwisko: &String) -> String{
+//     return ;
+// }
+// fn backwards(s0:&String){
+//     let mut s2 = String::new();
+//
+//     for c in s0.chars().rev() {
+//         s2.push(c);
+//     }
+//     println!("{}", s2);
+// }
 
 fn main() {
     // let mut j= 0;
@@ -59,19 +70,24 @@ fn main() {
     //lab2b::is_perf_num(496);
     //lab2b::prime_factors(84);
     //println!("{}",lab2b::pow_mod(5,3,13));
-    let x: i32 = 3;
-    let y: i32 = 2;
-    let z: i32 = 5;
+    // let x: i32 = 3;
+    // let y: i32 = 2;
+    // let z: i32 = 5;
     // println!("przed x:{x} y:{y}");
     // lab3::swap_two_args(&mut x, &mut y);
     // println!("po x:{x} y:{y}");
     //lab3::swap_sort_three_args(x, y, z);
-    let mut seed = 211537;
-    let min = 5;
-    let max = 15;
+    // let mut seed = 211537;
+    // let min = 5;
+    // let max = 15;
     //println!("{},{},{}", lab3::rand(&mut seed, min, max), lab3::rand(&mut seed, min, max), lab3::rand(&mut seed, min, max));
-    let mut arr: [i32;5] = [1,2,3,4,5];
-    println!("{:?}", arr);
-    lab3::swap_arr(&mut arr, 1, 5);
-    println!("{:?}", arr);
+    //let mut arr: [i32;5] = [1,2,3,4,5];
+    //println!("{:?}", arr);
+    //lab3::swap_arr(&mut arr, 1, 5);
+    //lab3::rand_perm(&mut arr, &mut seed);
+    //println!("{:?}", arr);
+    //let s0 = String::from("abc");
+    //backwards(&s0);
+    println!("sss");
+
 }
